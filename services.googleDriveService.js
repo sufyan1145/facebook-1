732b360bab4962ea1505660c608ad6e7fc5bf55b
sufyan1145 <sufyanaivideo2@gmail.com>
2026-07-17 23:18:54 +0500

    Add files via upload

--- services.googleDriveService.js
+++ services.googleDriveService.js
@@ -22,11 +22,16 @@ async function listFolders(userId, { search } = {}) {
     q += ` and name contains '${search.replace(/'/g, "\\'")}'`;
   }
 
+  logger.info(`[DEBUG] Drive query: ${q}`);
+
   const res = await drive.files.list({
     q,
     fields: 'files(id, name, owners, modifiedTime)',
     pageSize: 100,
   });
+
+  logger.info(`[DEBUG] Drive API status: ${res.status}, files returned: ${res.data.files ? res.data.files.length : 'undefined'}`);
+  logger.info(`[DEBUG] Drive API raw data: ${JSON.stringify(res.data)}`);
 
   const folders = [];
   for (const folder of res.data.files) {
