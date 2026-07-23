266a85843e97d827ea7b9d776199930d7306175d
sufyan1145 <sufyanaivideo2@gmail.com>
2026-07-23 15:25:59 +0500

    Add files via upload

--- config.env.js
+++ config.env.js
@@ -36,6 +36,7 @@ module.exports = {
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     redirectUri: process.env.GOOGLE_REDIRECT_URI,
     scopes: [
+      'https://www.googleapis.com/auth/drive.file',
       'https://www.googleapis.com/auth/drive.readonly',
       'https://www.googleapis.com/auth/userinfo.email',
       'https://www.googleapis.com/auth/userinfo.profile',
